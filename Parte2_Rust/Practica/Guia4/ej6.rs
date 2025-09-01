63a28e1b27f123e927e85fd0158d285e234b9091
RocioNavarro <56227691+RocioNavarro@users.noreply.github.com>
2025-09-01 14:25:27 -0300

    resolucion ej 6

---
+++ Parte2_Rust/Practica/Guia4/ej6.rs
@@ -0,0 +1,12 @@
+/*
+Escriba un programa que calcule el promedio de los números de una tupla de enteros de 32 bits. 
+El programa debe devolver el resultado en un formato que permita decimales. 
+En otras palabras, queremos que el promedio de (1, 3, 4, 6) sea 3.5 y no 3.
+*/
+
+fn main(){
+    let numeros = (1, 3, 4, 6);
+    let suma: i32 = numeros.0 + numeros.1 + numeros.2 + numeros.3;
+    let promedio: f32 = suma as f32 / 4.0; // Convierto la suma a f32 para obtener un resultado con decimales
+    println!("El promedio es: {}", promedio);
+}

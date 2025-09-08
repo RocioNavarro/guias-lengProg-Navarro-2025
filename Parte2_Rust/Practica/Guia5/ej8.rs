7fbde8ecbd7147c18882281db002dc989ab6ffa5
gabriel catalini <gabrielcatalini@MacBook-Air-de-gabriel.local>
2025-09-07 23:53:46 -0300

    Terminar de revisar el ej 8

---
+++ Parte2_Rust/Practica/Guia5/ej8.rs
@@ -0,0 +1,34 @@
+//El siguiente programa no compila. Explique el error y resuélvalo sin tocar el main.
+
+// Programa 8
+fn main() {
+    let s = give_ownership();
+    let _s = s.into_bytes(); // Consume el String (toma su ownership) y devuelve un Vec<u8>, la variable s ya no es válida
+    println!("{}", s); // s ya no existe --> ERROR: value borrowed here after move
+}
+
+/*
+¡Modificar únicamente el código abajo!
+fn give_ownership() -> String {
+    let s = String::from("hello, world");
+    s
+}
+*/
+
+
+/*
+SOLUCION:
+No consumit s, devolver un clon de s para que s siga siendo válido en main. 
+
+OBS:
+Devolver una referencia (&String) no es una opción válida aquí, porque la referencia dejaría de ser válida al salir de la función,
+ya que la variable s se destruye al finalizar la función give_ownership.
+*/
+
+fn give_ownership() -> String {
+    let s = String::from("hello, world");
+    s.clone() // Devolvemos una copia/clon del String, s sigue siendo válido en main
+}
+
+// todo arreglar --> sigue sin funcionar
+
